110952cb50a4025f8df239e63da0168f12e3e371
John Bell <jbellny@gmail.com>
2021-11-15 11:03:14 -0500

    Adding cart hide/show toggle to root reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,10 +7,12 @@ import { ReactComponent as Logo } from "../../assets/crown.svg"
 
 
 import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import "./header.styles.scss"
 
-const Header = ( { currentUser }) => (
+const Header = ( { currentUser, hidden }) => (
     <div className="header">
         <Link className="logo-ontainer" to="/">
             <Logo className="logo"/>
@@ -32,12 +34,17 @@ const Header = ( { currentUser }) => (
                     SIGN IN
                 </Link>
             }
+            <CartIcon />
         </div>
+        {
+            hidden ? null : <CartDropdown />
+        }
     </div>
 )
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header)
